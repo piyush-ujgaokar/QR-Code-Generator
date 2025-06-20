65d326331f25adb7636d931a141f6a2333fa22ab
piyush ujgaokar <piyushujgaokar544@gmail.com>
2025-06-20 13:12:43 +0530

    update ui

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var input = document.querySelector("#inp")
 var btn = document.querySelector("#butt")
 var qr_img = document.querySelector("img")
+let scan=document.querySelector(".scan")
 
 
 btn.addEventListener('click', () => {
@@ -8,11 +9,13 @@ btn.addEventListener('click', () => {
     let inpvalue = input.value;
     console.log(inpvalue);
 
+
     if (inpvalue == "") {
         alert("please enter the url")
     }else{
         
         qr_img.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inpvalue}`;
+        scan.style.display="block";
         qr_img.alt=`QR code for${inpvalue}`
     }
 });
